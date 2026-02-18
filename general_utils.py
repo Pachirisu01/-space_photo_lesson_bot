9f63458e166b4a1f13a8b309d40eaef2074f4dc5
Polina Vasilyeva <Polinova31@gmail.com>
2026-02-18 12:01:21 +0300

    add general_utils

---
+++ general_utils.py
@@ -0,0 +1,19 @@
+import os
+import requests
+
+
+def download_image(url, filepath):
+    try:
+        response = requests.get(url, timeout=30)
+        response.raise_for_status()
+
+        with open(filepath, 'wb') as f:
+            f.write(response.content)
+        return True
+    except Exception:
+        return False
+
+
+def get_file_extension(url):
+    _, ext = os.path.splitext(url.split("/")[-1])
+    return ext.lower() if ext else ".jpg"
